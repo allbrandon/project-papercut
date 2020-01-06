b0c7059351df2ab8ebd45cab43f48b263c41fc58
Cindy Zhou <cindy.zhou_work@hotmail.com>
2020-01-06 11:56:47 +1100

    changes to typescript format

--- src/components/ReceiptComponent/ItemComponent.tsx
+++ src/components/ReceiptComponent/ItemComponent.tsx
@@ -1,20 +1,24 @@
 import React from "react";
 import './Item.css';
 
-export const ItemComponent = ({ quantity, item, price, discount, discountamount, description }) => {
+function ItemComponentfunc (quantity: number, item: string, price: number, discount: string, discountamount: number, description: string) {
     return (
         <div>
             <div className="general-item-info">
-                <span className="circle">{ quantity }</span>
-                <span className="name">{ item }</span>
-                <span className="price">{ price.toFixed(2) }</span>
+                <span className="circle">{quantity}</span>
+                <span className="name">{item}</span>
+                <span className="price">{price.toFixed(2)}</span>
             </div>
             <div className="discount-info">
-                <span className="discount">{ discount }</span>
-                <span className="discount-amount">{ discountamount.toFixed(2) }</span>
+                <span className="discount">{discount}</span>
+                <span className="discount-amount">{discountamount.toFixed(2)}</span>
             </div>
-            <div className="description-info">{ description }</div>
+            <div className="description-info">{description}</div>
         </div>
         
     );
 };
+
+export const ItemComponent = ({ quantity, item, price, discount, discountamount, description }) => {
+    return ItemComponentfunc(quantity, item, price, discount, discountamount, description);
+}
