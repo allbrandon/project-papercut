9edc718e9a286c8cbe4cb437bda31f3fc17da337
Cindy Zhou <cindy.zhou_work@hotmail.com>
2020-01-14 16:23:26 +1100

    PP-80 changed itemcomponent to function component

--- src/components/ReceiptComponent/ItemComponent.tsx
+++ src/components/ReceiptComponent/ItemComponent.tsx
@@ -1,49 +1,26 @@
-import React from "react";
+import React, { FunctionComponent } from "react";
 import './Item.css';
 
-export interface itemInfo {
-    quantity: number;
-    item: string;
-    price: number;
-    discount: string;
-    discountamount: number;
-    description: string;
+type ItemInfo = {
+    quantity: number,
+    item: string,
+    price: number,
+    discount?: string,
+    discountamount?: number,
+    description?: string
 }
 
-function ItemComponentfunc (quantity: number, item: string, price: number, discount: string, discountamount: number, description: string) {
-    return (
-        <div>
-            <div className="general-item-info">
-                <span className="circle">{quantity}</span>
-                <span className="name">{item}</span>
-                <span className="price">{price.toFixed(2)}</span>
-            </div>
-            <div className="discount-info">
-                <span className="discount">{discount}</span>
-                <span className="discount-amount">{discountamount.toFixed(2)}</span>
-            </div>
-            <div className="description-info">{description}</div>
-        </div>
-        
-    );
-};
+export const ItemComponent: FunctionComponent<ItemInfo> = ({quantity, item, price, discount, discountamount, description}) => <div>
+    <div className="general-item-info">
+        <span className="circle">{quantity}</span>
+        <span className="name">{item}</span>
+        <span className="price">{price.toFixed(2)}</span>
+    </div>
 
-class ItemComponent extends React.Component<itemInfo> {
-    state = { loading: true };
+    <div className="discount-info">
+        <span className="discount">{discount}</span>
+        <span className="discount-amount">{discountamount.toFixed(2)}</span>
+    </div>
 
-    componentDidMount(){
-        this.setState({
-            loading: false
-        });
-    }
-
-    render(){
-        if (this.state.loading) {
-            return <h1>loading ...</h1>;
-        }
-
-        return ItemComponentfunc(this.props.quantity, this.props.item, this.props.price, this.props.discount, this.props.discountamount, this.props.description);
-    }
-}
-
-export default ItemComponent;
+    <div className="description-info">{description}</div>
+</div>
