6a9d075c0b589b49365a44651738dbb177be43e4
Cindy Zhou <cindy.zhou_work@hotmail.com>
2020-01-15 17:41:25 +1100

    PP-80 finalised and tested ItemComponent and ItemListComponent

--- src/components/ReceiptComponent/ItemComponent.tsx
+++ src/components/ReceiptComponent/ItemComponent.tsx
@@ -1,26 +1,26 @@
-import React, { FunctionComponent } from "react";
+import React from "react";
 import './Item.css';
 
-type ItemInfo = {
-    quantity: number,
-    item: string,
-    price: number,
-    discount?: string,
-    discountamount?: number,
-    description?: string
-}
+const ItemComponent = ({quantity, item, price, discount, discountamount, description}:{quantity:number, item: string,price: number,discount: string,discountamount: any,description: string}) => {
+    if(discountamount){
+        discountamount=(discountamount as number).toFixed(2);
+    }
+    return(
+        <div>
+            <div className="general-item-info">
+                <span className="circle">{quantity}</span>
+                <span className="name">{item}</span>
+                <span className="price">{price.toFixed(2)}</span>
+            </div>
 
-export const ItemComponent: FunctionComponent<ItemInfo> = ({quantity, item, price, discount, discountamount, description}) => <div>
-    <div className="general-item-info">
-        <span className="circle">{quantity}</span>
-        <span className="name">{item}</span>
-        <span className="price">{price.toFixed(2)}</span>
-    </div>
+            <div className="discount-info">
+                <span className="discount">{discount}</span>
+                <span className="discount-amount">{discountamount}</span>
+            </div>
 
-    <div className="discount-info">
-        <span className="discount">{discount}</span>
-        <span className="discount-amount">{discountamount.toFixed(2)}</span>
-    </div>
+            <div className="description-info">{description}</div>
+        </div>
+    );
+};
 
-    <div className="description-info">{description}</div>
-</div>
+export default ItemComponent;
