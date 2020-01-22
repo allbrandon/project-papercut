5990e0b39b806d8a124b4d9bd4d3a0c7b6d2d997
Cindy Zhou <cindy.zhou_work@hotmail.com>
2020-01-22 16:34:01 +1100

    PP-80 Performed react website test and appears to be fine XD

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
+// import ItemComponent from "./components/ReceiptComponent/ItemComponent";
 import ItemListComponent from "./components/ReceiptComponent/ItemListComponent";
 
 function App() {
   return (
     <Router>
+      {/* <Route exactpath="/" component={ItemComponent} /> */}
       <Route exactpath="/" component={ItemListComponent} />
     </Router>
   );
