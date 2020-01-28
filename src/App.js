3577b3482df3b87b748477b73c5a387179f5f967
Cindy Zhou <cindy.zhou_work@hotmail.com>
2020-01-28 14:47:02 +1100

    PP-80 changed colour in item.scss into variables

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
-// import ItemComponent from "./components/ReceiptComponent/ItemComponent";
-import ItemListComponent from "./components/ReceiptComponent/ItemListComponent";
+import ItemComponent from "./components/ReceiptComponent/ItemComponent";
+// import ItemListComponent from "./components/ReceiptComponent/ItemListComponent";
 
 function App() {
   return (
     <Router>
-      {/* <Route exactpath="/" component={ItemComponent} /> */}
-      <Route exactpath="/" component={ItemListComponent} />
+      <Route exactpath="/" component={ItemComponent} />
+      {/* <Route exactpath="/" component={ItemListComponent} /> */}
     </Router>
   );
 }
