3b5f723a9dc689a3ba828f8255503a73fbcc9052
Austin Vuong <austin.vuong00@gmail.com>
2020-01-30 21:16:03 +1100

    PP-101-Initial implementations of account settings

--- src/components/ProfileSettingsListComponent/ProfileSettingsListComponent.tsx
+++ src/components/ProfileSettingsListComponent/ProfileSettingsListComponent.tsx
@@ -2,13 +2,7 @@ import React from "react";
 import "./ProfileSettingsListComponent.scss";
 import { Link } from "react-router-dom";
 export const ProfileSettingsListComponent = ({}: any) => {
-  return (
-    <div className="settings__wrapper">
-      <Link to="/welcome">
-        <img src={process.env.PUBLIC_URL + "assets/papercut.svg"} alt=""></img>
-      </Link>
-    </div>
-  );
+  return <div className="settings__wrapper"></div>;
 };
 
 export default ProfileSettingsListComponent;
