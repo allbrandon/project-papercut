02628d2a2b8151c4d7e222b198b8d5968f87f302
Austin Vuong <austin.vuong00@gmail.com>
2020-02-03 00:01:03 +1100

    PP-101-Removed unnecessary files

--- src/App.js
+++ src/App.js
@@ -4,15 +4,12 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import SignUpComponent from "./components/SignUpComponent/SignUpComponent";
 import SignInComponent from "./components/SignInComponent/SignInComponent";
 import WelcomeComponent from "./components/WelcomeComponent/WelcomeComponent";
-import AccountProfileComponent from "./components/AccountProfileComponent/AccountProfileComponent";
-
 function App() {
   return (
     <Router>
       <Route exact path="/signup" component={SignUpComponent} />
       <Route path="/signin" component={SignInComponent} />
       <Route path="/welcome" component={WelcomeComponent} />
-      <Route path="/profile" component={AccountProfileComponent} />
     </Router>
   );
 }
