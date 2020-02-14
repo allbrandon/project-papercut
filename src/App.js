679724a79f0171dee3dc779258d374e944f22d3c
Austin Vuong <austin.vuong00@gmail.com>
2020-02-14 12:51:38 +1100

    PP-6-Fixed merge conflicts with master

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import './App.css';
 import MainScreen from './components/MainScreen.tsx'
@@ -11,20 +10,6 @@ function App() {
     </Router>
     
 
-=======
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import "./App.css";
-import ItemComponent from "./components/ReceiptComponent/ItemComponent";
-// import ItemListComponent from "./components/ReceiptComponent/ItemListComponent";
-
-function App() {
-  return (
-    <Router>
-      <Route exactpath="/" component={ItemComponent} />
-      {/* <Route exactpath="/" component={ItemListComponent} /> */}
-    </Router>
->>>>>>> master
   );
 }
 
